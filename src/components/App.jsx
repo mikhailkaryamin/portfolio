fe658ee431b7d4f90d8d8a694ed64572396f6bc5
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-08 21:50:10 +0500

    feat(App): create component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../sections/Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App () {
+  return(
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App;
