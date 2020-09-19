f4fe8dd9c12582ec4d6703e86ff09666b05819eb
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-19 20:19:51 +0500

    feat(Contact): create new section

---
+++ src/sections/Contact.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Row, Col, Form } from 'react-bootstrap';
+import emailjs from 'emailjs-com';
+
+import Section from '../components/Section';
+
+function Contact () {
+  function sendMail(e) {
+    e.preventDefault();
+    emailjs.sendForm('service_d9l3j4i', 'template_1cqv5dk', e.target, 'user_z0Ocda3PgJLn2z5fL9wSd')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <Section id="contact" title="Get In Touch">
+      <Row>
+        <Col md={4}>
+          <div className="contact-info">
+            <h3>Would you like to work with me? Awesome!</h3>
+            <p>
+                Don't like forms? Send me an <a href="mailto:karyamin.mihail@yandex.ru">email</a>.
+                <span role="img" aria-label="wave">👋</span>
+              </p>
+          </div>
+        </Col>
+        <Col md={8}>
+          <Form onSubmit={sendMail}>
+            <Row>
+              <Col md={6}>
+                <Form.Group controlId="inputName">
+                  <Form.Control
+                    required
+                    type="text"
+                    className="input"
+                    placeholder="Your name"
+                    name="from_name"
+                  />
+                </Form.Group>
+              </Col>
+              <Col md={6}>
+                <Form.Group controlId="inputEmail">
+                  <Form.Control
+                    required type="email"
+                    className="input"
+                    placeholder="Email address"
+                    name="email"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <Form.Group controlId="inputSubject">
+                  <Form.Control
+                    required
+                    type="text"
+                    className="input"
+                    placeholder="Subject"
+                    name="subject"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <Form.Group controlId="inputMessage">
+                  <Form.Control
+                    required
+                    as="textarea"
+                    rows={5}
+                    className="input"
+                    placeholder="Message"
+                    name="message"
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <button className="button" type="submit">
+                  Send message
+                </button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Section>
+  )
+}
+
+export default Contact;
