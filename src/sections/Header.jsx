a249deea5f0c72dd5257ad39ab1503f0c50d9f32
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-22 20:56:02 +0500

    fix(Header): remove link from logotype

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -9,44 +9,42 @@ function Header() {
     <header className="header fixed-top">
       <Container>
         <Navbar expand="lg" variant="dark">
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="Mikhail Karyamin" />
-          </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navigationBar" />
-        <Navbar.Collapse id="navigationBar">
-          <Nav className="ml-auto" as="ul">
-            <Nav.Item as="li">
-              <SmoothScroll href="#home" className="nav-link">
-                Home
-              </SmoothScroll>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <SmoothScroll href="#about" className="nav-link">
-                About
-              </SmoothScroll>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <SmoothScroll href="#certifications" className="nav-link">
-                Cert
-              </SmoothScroll>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <SmoothScroll href="#experience" className="nav-link">
-                Experience
-              </SmoothScroll>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <SmoothScroll href="#tools" className="nav-link">
-                Tools
-              </SmoothScroll>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <SmoothScroll href="#contact" className="nav-link">
-                Contact
-              </SmoothScroll>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
+          <img src={logo} alt="Mikhail Karyamin" />
+          <Navbar.Toggle aria-controls="navigationBar" />
+          <Navbar.Collapse id="navigationBar">
+            <Nav className="ml-auto" as="ul">
+              <Nav.Item as="li">
+                <SmoothScroll href="#home" className="nav-link">
+                  Home
+                </SmoothScroll>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <SmoothScroll href="#about" className="nav-link">
+                  About
+                </SmoothScroll>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <SmoothScroll href="#certifications" className="nav-link">
+                  Cert
+                </SmoothScroll>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <SmoothScroll href="#experience" className="nav-link">
+                  Experience
+                </SmoothScroll>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <SmoothScroll href="#tools" className="nav-link">
+                  Tools
+                </SmoothScroll>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <SmoothScroll href="#contact" className="nav-link">
+                  Contact
+                </SmoothScroll>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </Navbar>
       </Container>
     </header>
