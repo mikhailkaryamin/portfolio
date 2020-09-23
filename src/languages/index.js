1e2b83a95626da3879823624b57221bf20b12fb7
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-09-23 19:59:52 +0500

    feat(index lang):

---
+++ src/languages/index.js
@@ -0,0 +1,16 @@
+import en from './en.json';
+import ru from './ru.json';
+
+const dictionaryList = {
+  en,
+  ru,
+};
+const languageOptions = {
+  en: 'English',
+  ru: 'Русский',
+};
+
+export {
+  dictionaryList,
+  languageOptions,
+}
