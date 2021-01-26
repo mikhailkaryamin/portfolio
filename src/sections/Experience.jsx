01193d5cd45dfb61906a2bfc39f612a78f5cf608
mikhailkaryamin <karyamin.mihail@yandex.ru>
2021-01-26 20:28:19 +0300

    chore(Experience): change cards

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -6,22 +6,12 @@ import ButtonLink from '../components/ButtonLink';
 import CardExperience from '../components/CardExperience';
 import Section from '../components/Section';
 
-import nerdsPreview from '../assets/preview-nerds.jpg';
-import citiesPreview from '../assets/preview-cities.jpg';
-import kekstagramPreview from '../assets/preview-kekstagram.jpg';
 import mishkaPreview from '../assets/preview-mishka.jpg';
-import taskmanagerPreview from '../assets/preview-taskmanager.jpg';
-import wtwPreview from '../assets/preview-wtw.jpg';
+import pinkPreview from '../assets/preview-pink.jpg';
+import pictureMatchPreview from '../assets/preview-picture-match.jpg';
+import countDayPreview from '../assets/preview-count-day.jpg';
 
 const EXPERIENCE_LIST = [
-  {
-    title: 'Nerds',
-    description: <Text section="cardExperience" value="nerds" />,
-    technologies: ['HTML', 'CSS', 'JS', 'Fixed'],
-    repoLink: 'https://github.com/mikhailkaryamin/nerds',
-    projectLink: 'https://mikhailkaryamin.github.io/nerds/',
-    preview: nerdsPreview,
-  },
   {
     title: 'Mishka',
     description: <Text section="cardExperience" value="mishka" />,
@@ -31,37 +21,29 @@ const EXPERIENCE_LIST = [
     preview: mishkaPreview,
   },
   {
-    title: 'Kekstagram',
-    description: <Text section="cardExperience" value="kekstagram" />,
-    technologies: ['JS ES5'],
-    repoLink: 'https://github.com/mikhailkaryamin/kekstagram',
+    title: 'Pink',
+    description: <Text section="cardExperience" value="pink" />,
+    technologies: ['HTML', 'CSS', 'JS', 'Adaptive' ],
+    repoLink: 'https://github.com/mikhailkaryamin/pink',
+    projectLink: 'https://mikhailkaryamin.github.io/pink/',
+    preview: pinkPreview,
+  },
+  {
+    title: 'Picture match',
+    description: <Text section="cardExperience" value="pictureMatch" />,
+    technologies: ['React', 'Redux', 'TypeScript'],
+    repoLink: 'https://github.com/mikhailkaryamin/picture-match',
+    projectLink: 'https://mikhailkaryamin.github.io/picture-match/',
+    preview: pictureMatchPreview,
+  },
+  {
+    title: 'Count day',
+    description: <Text section="cardExperience" value="countDay" />,
+    technologies: ['React-native', 'useContext+useReducer', 'TypeScript'],
+    repoLink: 'https://github.com/mikhailkaryamin/count-day',
     projectLink: '',
-    preview: kekstagramPreview,
+    preview: countDayPreview,
   },
-  {
-    title: 'Task manager',
-    description: <Text section="cardExperience" value="taskManager" />,
-    technologies: ['JS ES6+'],
-    repoLink: 'https://github.com/mikhailkaryamin/tasks-manager',
-    projectLink: 'https://mikhailkaryamin.github.io/tasks-manager/',
-    preview: taskmanagerPreview,
-  },
-  {
-    title: 'Six cities',
-    description: <Text section="cardExperience" value="sixCities" />,
-    technologies: ['React', 'Redux', 'TypeScript'],
-    repoLink: 'https://github.com/mikhailkaryamin/959581-six-cities-3',
-    projectLink: 'https://mikhailkaryamin.github.io/959581-six-cities-3/',
-    preview: citiesPreview,
-  },
-  {
-    title: 'What to watch',
-    description: <Text section="cardExperience" value="whatToWatch" />,
-    technologies: ['React', 'Redux', 'TypeScript'],
-    repoLink: 'https://github.com/mikhailkaryamin/what-to-watch',
-    projectLink: 'https://mikhailkaryamin.github.io/what-to-watch/',
-    preview: wtwPreview,
-  }
 ];
 
 const ButtonShowMore = {
@@ -90,7 +72,7 @@ function Experience () {
               className="mb-4 d-flex"
               key={`${experience}-${i}`}
             >
-              <CardExperience 
+              <CardExperience
                 title={title}
                 description={description}
                 repoLink={repoLink}
